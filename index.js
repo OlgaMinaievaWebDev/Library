9e0c49a94e39fb758646ae29a05ea59536bf9adb
Olga <minaeva9@gmail.com>
2023-11-26 18:21:28 -0500

    form styled

--- index.js
+++ index.js
@@ -4,18 +4,9 @@ function Book(title, author, pages, read) {
   (this.title = title),
     (this.author = author),
     (this.pages = pages),
-    (this.read = read),
+    (this.read = read);
 }
-
 const addBook = document.getElementById("addBook");
 
-
-
 //button opens the dialog
-addBook.addEventListener("click", function () {
- dialog.showModal();
-});
-
-
-
-
+addBook.addEventListener("click", function () {});
