e9958c7bbeee6ce6014ce97b49e9e9979dd94268
Olga <minaeva9@gmail.com>
2023-11-26 18:34:55 -0500

    event listener to add book

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ function Book(title, author, pages, read) {
     (this.pages = pages),
     (this.read = read);
 }
+
+function addBookToLibrary() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+  let newBook = new Book(title, author, pages, read);
+  console.log(newBook);
+}
 const addBook = document.getElementById("addBook");
+const form = document.getElementById("modal");
+const submitBook = document.getElementById("submit");
 
 //button opens the dialog
-addBook.addEventListener("click", function () {});
+addBook.addEventListener("click", function () {
+  form.style.display = "flex";
+});
+
+// submit new book to the library
+submitBook.addEventListener("click", function () {
+  event.preventDefault();
+  addBookToLibrary();
+});
