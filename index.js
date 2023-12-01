0f50df57e34f1d1a3d03375ee1c39c04179e66aa
Olga <minaeva9@gmail.com>
2023-12-01 16:49:37 -0500

    modified

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ function Book(title, author, pages, read) {
     (this.read = read);
 }
 
+//toggle read/not read
+
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
+
+function toggleRead(index) {
+  myLibrary[index].toggleRead();
+  displayBook();
+}
+
+
+
 function addBookToLibrary() {
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
@@ -14,7 +27,7 @@ function addBookToLibrary() {
   let read = document.getElementById("read").checked;
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-  console.log(myLibrary);
+
   displayBook();
 }
 
@@ -29,9 +42,12 @@ function displayBook() {
     bookEl.innerHTML = `<h2>${book.title}</h2>
     <h4>${book.author}</h4>
     <h4>${book.pages}</h4>
-    <button>${book.read ? "Read" : "Not Read"}</button>
+    <button class="toggle"  onclick="toggleRead(${i})">${
+      book.read ? "Read"  : "Not Read"
+    }</button>
     <button class="remove" onclick="removeBook(${i})">Remove</button>`;
     libraryEl.appendChild(bookEl);
+
     form.style.display = "none";
   }
 }
